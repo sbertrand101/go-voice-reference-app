8457a24cd84e96d746a2c3e15a790a629884ead0
Andrey Belchikov <belchikovs@gmail.com>
2016-05-04 13:42:00 +0300

    get database options from env variable

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 )
 
 func main() {
 	router := gin.Default()
-	db, err := gorm.Open("postgres", "GoLangVoiceReferenceApp")
+	databaseSource := os.Getenv("DATABASE")
+	if databaseSource == "" {
+		databaseSource = "GoLangVoiceReferenceApp"
+	}
+	db, err := gorm.Open("postgres", databaseSource)
 	if err != nil {
 		panic("Failed to connect database")
 	}
