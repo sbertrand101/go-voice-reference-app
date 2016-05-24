e35269aab923adbba074bf49210863c8bc890d5b
Andrey Belchikov <belchikovs@gmail.com>
2016-05-24 17:27:35 +0300

    Updated dependencies

--- vendor/github.com/bandwidthcom/go-bandwidth/xml/hangup.go
+++
@@ -1,8 +0,0 @@
-package xml
-
-import "encoding/xml"
-
-// The Hangup verb is used to hangup current call
-type Hangup struct {
-	XMLName xml.Name `xml:"Hangup"`
-}
--- vendor/github.com/bandwidthcom/go-bandwidth/xml/reject.go
+++
@@ -1,9 +0,0 @@
-package xml
-
-import "encoding/xml"
-
-// Reject verb is used to reject incoming calls
-type Reject struct {
-	XMLName xml.Name `xml:"Reject"`
-	Reason  string   `xml:"reason,attr,omitempty"`
-}
